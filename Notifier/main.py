b76345e9bc1314a52d02c818a24273369c150511
Matteo Bini <mail@matteobini.me>
2022-09-05 22:28:50 +0200

    Refactor to pep8 standard

--- Notifier/main.py
+++ Notifier/main.py
@@ -1,68 +1,58 @@
-from src.databaseOperations import getSubscribers, db_notification
-from src.telegramOperations import register_new_user, tg_notification
-from src.emailOperations import *
-from src.fermiCalendar import *
-from src.utility import *
+from src.databaseOperations import get_subscribers, store_notification
+from src.telegramOperations import register_new_telegram_user, tg_notification
+from src.emailOperations import pending_registration, welcome_notification, \
+                                email_notification
+from src.fermiCalendar import collect_notifications
 
-def deliver_notification(n):
-    # This function delegates the operations to
-    # be done when I decide to notify someone: 
-    # send email, send messaage and store the 
-    # notification
-    email_notification(n)
-    tg_notification(n)
-    db_notification(n["id"], n["events"])
+"""
+Main.
+"""
 
+def deliver_notification(notification: dict) -> None:
+    """This function delegates the notification delivery to the appropriate 
+    functions.
+
+    Such as:
+    - Send email notification.
+    - Send telegram notification.
+    - Store the notification in the database.
+
+    Args:
+        notification (list): list of dictionaries containing all the 
+        subscribers that need to be notified and
+        the corresponding events.
+    """
+    email_notification(notification)
+    tg_notification(notification)
+    store_notification(notification["id"], notification["events"])
     return
 
-def main(last_update_id):
+def main():
+    """Being the main function of the program, it will call all the other 
+    functions.
 
+    Such as:
+    - Handling the pending registrations.
+    - Collecting the notifications.
+    - Deliver the notifications.
+    """
     while True:
-        ###           COLLECT SUBSCRIBERS DATA          ###
-        subs = getSubscribers()
+        # Collect all the subscribers data from
+        # the database
+        subs = get_subscribers()
+
+        pending_registration(subs)  # Send the confirmation email
+        welcome_notification(subs)  # Welcome the new users
+        register_new_telegram_user(subs)
         
-        ###           USER REGISTRATION EVENTS          ###
-        pending_registration(subs)
-        welcome_notification(subs)
-
-        ###        TELEGRAM REGISTRATION EVENTS         ###
-        last_update_id = register_new_user(subs, last_update_id)
-
-        ###        COLLECT & SEND NOTIFICATIONS         ###
+        # Collect all the new notifications
         notifications = collect_notifications(subs)
-        for user in notifications:
-            deliver_notification(user)
 
-    return "merda."
+        for user in notifications:
+            deliver_notification(user)  # Deliver the notifications.
+
+    return "fuck."
 
 
 if __name__ == "__main__":
-    main(last_update_id=0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    main()
